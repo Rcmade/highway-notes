ba6c9315e3b98e82f0c803bbb4538c8376753dee
Rcmade <rahulchourasiya4567@gmail.com>
2025-09-02 18:08:16 +0530

    hotfix: remove extra space in auth layout

--- frontend/src/features/auth/components/form/AuthLayout.tsx
+++ frontend/src/features/auth/components/form/AuthLayout.tsx
@@ -9,7 +9,7 @@ const AuthLayout = () => {
         <img src="/images/logo.png" className="mr-3 size-10 object-contain" />
         HD
       </div>
-      <section className="flex h-svh gap-8 p-2 pt-16">
+      <section className="flex h-svh gap-8 p-2 pt-16 md:pt-2">
         {/* Form */}
         <div className="md:flex-rows flex h-full w-full flex-col items-center gap-4 overflow-y-auto pt-12 md:w-1/2 md:items-center md:justify-center md:px-6 md:pt-0 lg:w-[45%]">
           <div className="flex items-center gap-2 text-lg font-bold md:hidden">
